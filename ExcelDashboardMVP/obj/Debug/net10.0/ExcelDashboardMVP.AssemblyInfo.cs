90222f3fe2ab720c61aa1ead62e6e21b03b482d5
Malachi Mathins <56065996+EmperorKaido99@users.noreply.github.com>
2026-02-23 09:36:07 +0000

    updated the dashboards

--- ExcelDashboardMVP/obj/Debug/net10.0/ExcelDashboardMVP.AssemblyInfo.cs
+++ ExcelDashboardMVP/obj/Debug/net10.0/ExcelDashboardMVP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExcelDashboardMVP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69e0c116742d8cd9723c1e6104e39a42d1a088b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+019a88c650fcf4d418f413ced4e32316192d6259")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExcelDashboardMVP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExcelDashboardMVP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
